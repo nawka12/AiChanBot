9a731544583d0f647ea4690460c28b4d9bac4ee6
nawka12 <54880732+nawka12@users.noreply.github.com>
2023-04-24 13:24:17 +0700

    Reverting back to CommonJS and fixes
    Date fix, adding back `ai!` (without search) and moving current `ai!` to `aisearch!`
    adding sendTyping so you know that the bot is responding(?)

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { searchQuery } from './searchlogic.js';
-import whitelist from './whitelist.json' assert { type: 'json' };
+require('dotenv').config();
 
-import { Client } from 'discord.js';
-import Discord from 'discord.js';
-const { GatewayIntentBits } = Discord;
+const { searchQuery } = require('./searchlogic.js');
+const whitelist = require('./whitelist.json');
+
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
-]})
+]});
 
-import { Configuration , OpenAIApi } from 'openai';
+const { Configuration , OpenAIApi } = require('openai');
 const configuration = new Configuration({
     organization: process.env.OPENAI_ORG,
     apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const date = new Date();
 const options = { day: 'numeric', month: 'long', year: 'numeric' };
-const currentDate = date.toLocaleDateString('en-US', options);
 var lastResponse = "";
 
 client.on('messageCreate', async function(message){
@@ -32,12 +28,13 @@ client.on('messageCreate', async function(message){
         }
 
         if(message.content.toLowerCase() == "aireset!"){
+            message.channel.sendTyping();
             lastResponse = "";
             message.reply("Ai-chan's last respond has been reset.");
             return;
         }
 
-        if(message.author.bot || !message.content.toLowerCase().startsWith("ai!") && !message.content.toLowerCase().startsWith("aideep!") || message.content.toLowerCase() == ("ai!") || message.content.toLowerCase() == ("aideep!") ) return;
+        if(message.author.bot || !message.content.toLowerCase().startsWith("aisearch!") && !message.content.toLowerCase().startsWith("aideep!") && !message.content.toLowerCase().startsWith("ai!") || message.content.toLowerCase() == ("aisearch!") || message.content.toLowerCase() == ("aideep!") || message.content.toLowerCase() == ("ai!") ) return;
         if(!whitelist.list.includes(message.author.id)){
             message.reply(`You're not authorized to use me.`);
             return;
@@ -51,13 +48,13 @@ client.on('messageCreate', async function(message){
             message.reply(`Your input is inappropriate. I will not respond to that.`);
             return;
         }
-
+        
         if(message.content.toLowerCase().startsWith("aideep!")){
             try {
                 const deepInput = input.slice(4);
                 const searchResult = await searchQuery(`${deepInput}`);
                 const messageDeep = [
-                    { role: "system", content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${currentDate}.` },
+                    { role: "system", content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}.` },
                     { role: "system", content: `Here's more data from the web about the user's question:`}
                 ];
 
@@ -72,6 +69,7 @@ client.on('messageCreate', async function(message){
                   );
 
                 // console.log(messageDeep);
+                message.channel.sendTyping();
 
                 const gptResponse = await openai.createChatCompletion({
                     model: "gpt-3.5-turbo",
@@ -94,13 +92,15 @@ client.on('messageCreate', async function(message){
             }
         }
 
-        try {
-            const searchResult = await searchQuery(`${input}`);
+        if(message.content.toLowerCase().startsWith("aisearch!")){
+          try {
+            const searchInput = input.slice(6);
+            const searchResult = await searchQuery(`${searchInput}`);
             const results = searchResult.results.slice(0, 2);
             const messages = [
               {
                 role: "system",
-                content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${currentDate}.`,
+                content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}.`,
               },
               {
                 role: "system",
@@ -117,7 +117,7 @@ client.on('messageCreate', async function(message){
               { role: "assistant", content: `${lastResponse}` },
               { role: "user", content: `${input}` }
             );
-            
+            message.channel.sendTyping();
             // console.log(messages);
             const gptResponse = await openai.createChatCompletion({
               model: "gpt-3.5-turbo",
@@ -136,7 +136,38 @@ client.on('messageCreate', async function(message){
             console.error(error);
             message.reply(`There was an error processing your request.`);
             return;
-          }          
+          }
+        }
+        
+        if(message.content.toLowerCase().startsWith("ai!")){
+          try {
+            const messages = [
+              { role: "system", content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}.` },
+              { role: "assistant", content: `${lastResponse}` },
+              { role: "user", content: `${input}` }
+            ];
+            message.channel.sendTyping();
+            // console.log(messages);
+            const gptResponse = await openai.createChatCompletion({
+              model: "gpt-3.5-turbo",
+              messages,
+              temperature: 0.4,
+              max_tokens: 256,
+            });
+          
+            lastResponse = gptResponse.data.choices[0].message.content;
+            const totalTokens = gptResponse.data.usage.total_tokens;
+            const cost = (totalTokens * 0.000002).toFixed(6);
+            
+            message.reply(
+              `${lastResponse}\n\n\`\`\`Token Used: ${totalTokens}\nCost: $${cost}\`\`\``
+            );
+          } catch (error) {
+            console.error(error);
+            message.reply(`There was an error processing your request.`);
+            return;
+          }
+        }
 } catch (err) {
   console.log(err)
 }
