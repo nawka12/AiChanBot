24b95eec4f97ff4df070669773d6e62f68a7321f
KayfaHaarukku <54880732+nawka12@users.noreply.github.com>
2024-06-27 17:14:56 +0700

    Migrate to Claude

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ const client = new Client({
     ]
 });
 
-const OpenAI = require('openai');
-
-const openai = new OpenAI({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_KEY,
+const Anthropic = require('@anthropic-ai/sdk');
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
 });
 
 const options = { day: 'numeric', month: 'long', year: 'numeric' };
@@ -42,105 +40,37 @@ client.on('messageCreate', async function(message) {
                 return;
             }
 
-            // if (!whitelist.list.includes(message.author.id)) {
-            //     message.reply(`You're not authorized to use me.`);
-            //     return;
-            // }
+            // Removed whitelist check and moderation (Anthropic has built-in content filtering)
 
-            const moderation = await openai.moderations.create({
-                input: `${commandContent}`
-            });
-
-            if (moderation.results[0].flagged) {
-                message.reply(`Your input is inappropriate. I will not respond to that.`);
-                return;
-            }
-
-            if (command === 'deepsearch') {
+            if (command === 'deepsearch' || command === 'search') {
                 try {
                     const searchResult = await searchQuery(commandContent);
-                    const messageDeep = [
-                        { role: "system", content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}. You have 3 modes; offline, search (connects you to the internet with up to 3 search results), and deepsearch (connects you to the internet with up to 10 search results). You're connected to the internet with deepsearch command. Keep your answer as short as possible.` },
+                    const results = command === 'deepsearch' ? searchResult.results.slice(0, 10) : searchResult.results.slice(0, 3);
+                    const messages = [
+                        { role: "system", content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}. You have 3 modes; offline, search (connects you to the internet with up to 3 search results), and deepsearch (connects you to the internet with up to 10 search results). You're connected to the internet with ${command} command. Keep your answer as short as possible.` },
                         { role: "system", content: `Here's more data from the web about the user's question:` },
                         ...userConversations[message.author.id] || []
                     ];
 
-                    for (let i = 0; i < Math.min(searchResult.results.length, 10); i++) {
-                        const { url, title, content } = searchResult.results[i];
-                        messageDeep.push({ role: "system", content: `URL: ${url}, Title: ${title}, Content: ${content}` });
-                    }
-
-                    messageDeep.push({ role: "user", content: `${commandContent}` });
-
-                    const gptResponse = await openai.chat.completions.create({
-                        model: "gpt-4o",
-                        messages: messageDeep,
-                        temperature: 1.0,
-                        max_tokens: 256,
-                    });
-
-                    if (!userConversations[message.author.id]) {
-                        userConversations[message.author.id] = [];
-                    }
-                    userConversations[message.author.id].push({ role: "user", content: commandContent });
-                    userConversations[message.author.id].push({ role: "assistant", content: gptResponse.choices[0].message.content });
-
-                    const promptTokens = gptResponse.usage.prompt_tokens;
-                    const completionTokens = gptResponse.usage.completion_tokens;
-                    const totalTokens = gptResponse.usage.total_tokens;
-                    const cost = ((promptTokens * 0.000005) + (completionTokens * 0.000015)).toFixed(6);
-
-                    message.reply(`${gptResponse.choices[0].message.content}\n\n\`\`\`Token Used: ${totalTokens}\nCost: $${cost}\`\`\``);
-                    return;
-                } catch (error) {
-                    console.error(error);
-                    message.reply(`There was an error processing your request.`);
-                    return;
-                }
-            }
-
-            if (command === 'search') {
-                try {
-                    const searchResult = await searchQuery(commandContent);
-                    const results = searchResult.results.slice(0, 2);
-                    const messages = [
-                        {
-                            role: "system",
-                            content: `You are Ai-chan, a helpful assistant in a form of Discord bot. Your name is taken from Kizuna Ai, a virtual YouTuber. Today is ${new Date().toLocaleDateString('en-US', options)}. You have 3 modes; offline, search (connects you to the internet with up to 3 search results), and deepsearch (connects you to the internet with up to 10 search results). You're connected to the internet with search command. Keep your answer as short as possible.`,
-                        },
-                        {
-                            role: "system",
-                            content: `Here's more data from the web about the user's question:`,
-                        },
-                        ...userConversations[message.author.id] || []
-                    ];
                     results.forEach((result) => {
-                        messages.push({
-                            role: "system",
-                            content: `URL: ${result.url}, Title: ${result.title}, Content: ${result.content}`,
-                        });
+                        messages.push({ role: "system", content: `URL: ${result.url}, Title: ${result.title}, Content: ${result.content}` });
                     });
+
                     messages.push({ role: "user", content: `${commandContent}` });
 
-                    const gptResponse = await openai.chat.completions.create({
-                        model: "gpt-4o",
-                        messages,
-                        temperature: 1.0,
+                    const response = await anthropic.messages.create({
+                        model: "claude-3-5-sonnet-20240620",
                         max_tokens: 256,
+                        messages: messages,
                     });
 
                     if (!userConversations[message.author.id]) {
                         userConversations[message.author.id] = [];
                     }
                     userConversations[message.author.id].push({ role: "user", content: commandContent });
-                    userConversations[message.author.id].push({ role: "assistant", content: gptResponse.choices[0].message.content });
+                    userConversations[message.author.id].push({ role: "assistant", content: response.content[0].text });
 
-                    const promptTokens = gptResponse.usage.prompt_tokens;
-                    const completionTokens = gptResponse.usage.completion_tokens;
-                    const totalTokens = gptResponse.usage.total_tokens;
-                    const cost = ((promptTokens * 0.000005) + (completionTokens * 0.000015)).toFixed(6);
-
-                    message.reply(`${gptResponse.choices[0].message.content}\n\n\`\`\`Token Used: ${totalTokens}\nCost: $${cost}\`\`\``);
+                    message.reply(response.content[0].text);
                     return;
                 } catch (error) {
                     console.error(error);
@@ -156,25 +86,19 @@ client.on('messageCreate', async function(message) {
                 { role: "user", content: `${input}` }
             ];
 
-            const gptResponse = await openai.chat.completions.create({
-                model: "gpt-4o",
-                messages,
-                temperature: 1.0,
+            const response = await anthropic.messages.create({
+                model: "claude-3-5-sonnet-20240620",
                 max_tokens: 256,
+                messages: messages,
             });
 
             if (!userConversations[message.author.id]) {
                 userConversations[message.author.id] = [];
             }
             userConversations[message.author.id].push({ role: "user", content: input });
-            userConversations[message.author.id].push({ role: "assistant", content: gptResponse.choices[0].message.content });
+            userConversations[message.author.id].push({ role: "assistant", content: response.content[0].text });
 
-            const promptTokens = gptResponse.usage.prompt_tokens;
-            const completionTokens = gptResponse.usage.completion_tokens;
-            const totalTokens = gptResponse.usage.total_tokens;
-            const cost = ((promptTokens * 0.000005) + (completionTokens * 0.000015)).toFixed(6);
-
-            message.reply(`${gptResponse.choices[0].message.content}\n\n\`\`\`Token Used: ${totalTokens}\nCost: $${cost}\`\`\``);
+            message.reply(response.content[0].text);
         }
     } catch (err) {
         console.log(err);
